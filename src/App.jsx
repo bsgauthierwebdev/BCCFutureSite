331968b3aba738c9db9d7d374d054786f9e10c44
Brent Gauthier <cajundrummer@gmail.com>
2024-11-10 16:52:07 -0500

    Import Organizers component into App and set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomeView from './components/HomeView/HomeView'
 import './App.css'
+import Organizers from "./components/Organizers/Organizers"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<HomeView />} />
+				<Route path = '/organizers' element = {<Organizers />} />
 			</Routes>
 		</Router>
 	)
